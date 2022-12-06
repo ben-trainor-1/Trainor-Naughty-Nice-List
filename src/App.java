9494f383caa47f1537a7af257edc306a851c8fb8
Ben Trainor <benjammintrain@gmail.com>
2022-12-06 09:23:37 -0600

    first commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        String naughtyNiceList[][] = {
+            {"LoGaN", "may", "Naughty", "7730 Whitemarsh Court", "deOdeRant"},
+            {"Cecilia", "bOYer", "Nice", "8686 Elm St.", "PUddle"},
+            {"lorElAi", "moRsE", "Naughty", "8529 Birchpond St.", "TOMAto"},
+            {"rory", "black", "Naughty", "39 Fairfield Ave.", "lamp ShaDe"},
+            {"Denzel", "SancHez", "", "961 Hanover Ave.", "shOEs"},
+            {"maNUEL", "Lambert", "Nice", "698 Arrowhead Rd.", "chArgEr"},
+            {"Trinity", "FISHER", "Nice", "49 Arlington Avenue", "winDow"},
+            {"maTthiAs", "hayEs", "Nice", "23 Woodside Ave.", "LOTION"},
+            {"Sophia", "SToUT", "Naughty", "7640 Andover Dr.", "teddY beAr"},
+            {"keNNedy", "Dunlap", "", "63 Cypress Rd.", "pAnts"}
+        };
+
+    }
+}
